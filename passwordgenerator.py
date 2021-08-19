ae8979c885a49a0ea25ff961d9263afb7855dd19
Matt Calvert <64328076+XanderCalvert@users.noreply.github.com>
2021-08-19 09:10:57 +0100

    comments
    uncomment second solution

--- passwordgenerator.py
+++ passwordgenerator.py
@@ -22,24 +22,24 @@ str_var = list(password)
 random.shuffle(str_var)
 print("".join(str_var))
 
-## Alt
-# password_list = []
-#
-# for char in range(1, nr_letters + 1):
-#   password_list.append(random.choice(letters))
-#
-# for char in range(1, nr_symbols + 1):
-#   password_list += random.choice(symbols)
-#
-# for char in range(1, nr_numbers + 1):
-#   password_list += random.choice(numbers)
-#
-# print(password_list)
-# random.shuffle(password_list)
-# print(password_list)
-#
-# password = ""
-# for char in password_list:
-#   password += char
-#
-# print(f"Your password is: {password}")
+# Alt
+password_list = []
+
+for char in range(1, nr_letters + 1):
+  password_list.append(random.choice(letters))
+
+for char in range(1, nr_symbols + 1):
+  password_list += random.choice(symbols)
+
+for char in range(1, nr_numbers + 1):
+  password_list += random.choice(numbers)
+
+print(password_list)
+random.shuffle(password_list)
+print(password_list)
+
+password = ""
+for char in password_list:
+  password += char
+
+print(f"Your password is: {password}")
